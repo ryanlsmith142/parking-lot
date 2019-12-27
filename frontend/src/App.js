9d502fa9fa880734b1f7ceb70ceccee3707244ff
Ryan Smith <ryan.l.smith142@gmail.com>
2019-12-27 16:23:06 -0600

    played around with running front end and back end, had trouble with controllers and integrating with front end react component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {};
+
+    componentDidMount() {
+        setInterval(this.hello, 250);
+    }
+
+    hello = () => {
+        fetch('api/hello')
+            .then(response => response.text())
+            .then(message => {
+                this.setState({message: message});
+            });
+    };
+
+    render() {
+        return (
+            <div className="app">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">{ this.state.message }</h1>
+                </header>
+                <p className="App-intro">
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/main/java/com/parkinglot/parkinglot/controllers/HelloController.java
+++ src/main/java/com/parkinglot/parkinglot/controllers/HelloController.java
@@ -2,16 +2,15 @@ package com.parkinglot.parkinglot.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("api/hello")
     public String hello() {
-        return "Hello " + new Date();
+        return "Hello from Spring " + new Date();
     }
 
 }
