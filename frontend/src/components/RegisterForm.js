cb3bd82ecefd030f9f458d37d5140942b1c9f745
Ryan Smith <ryan.l.smith142@gmail.com>
2019-12-28 15:04:22 -0600

    add RegisterForm component

---
+++ frontend/src/components/RegisterForm.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class RegisterForm extends React.Component {
+    state = {
+        username: '',
+        password: '',
+        email: '',
+        isAdmin: false,
+        profilePic: '',
+        firstName: '',
+        lastName: ''
+    };
+
+    registerButton = event => {
+        event.preventDefault();
+
+        this.setState({value: event.target.value})
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input
+                        type='text'
+                        value={this.state.username}
+                        onChange={this.registerButton}
+                    />
+                    <input
+                        type='submit'
+                    />
+                </form>
+            </div>
+        )
+    }
+}
