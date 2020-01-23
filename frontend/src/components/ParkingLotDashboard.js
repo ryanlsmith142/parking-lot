15ec46ab26c79f0cc06d851dfd50b59d80c0dc5f
Ryan Smith <ryan.l.smith142@gmail.com>
2020-01-23 09:45:57 -0600

    add classes to divs

--- frontend/src/components/ParkingLotDashboard.js
+++ frontend/src/components/ParkingLotDashboard.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import CreateQuestionForm from './CreateQuestionForm.js';
 import QuestionList from './QuestionList.js';
+import '../parkingLotDashboard.css';
 
 class ParkingLotDashboard extends React.Component {
     render() {
         return (
-            <div>
-                <QuestionList />
-                <CreateQuestionForm/>
+            <div className="main-div">
+                <header className="dashboard-header">
+                    <h1 className="dashboard-h1">The Parking Lot</h1>
+                    <ul className="dashboard-nav">
+                        <li>Create Question</li>
+                        <li>Show Questions</li>
+                    </ul>
+                </header>
+                <div className="question-list">
+                    <QuestionList />
+                </div>
+                <div className="create-question-form">
+                    <CreateQuestionForm/>
+                </div>
             </div>
         )
     }
