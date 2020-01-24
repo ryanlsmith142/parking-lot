82a2424eba5a1f92b6648299f61bff0627875735
Ryan Smith <ryan.l.smith142@gmail.com>
2020-01-24 17:11:41 -0600

    add more styling

--- frontend/src/components/ParkingLotDashboard.js
+++ frontend/src/components/ParkingLotDashboard.js
@@ -6,7 +6,7 @@ import '../parkingLotDashboard.css';
 class ParkingLotDashboard extends React.Component {
     render() {
         return (
-            <div className="main-div">
+            <div className="header-div">
                 <header className="dashboard-header">
                     <h1 className="dashboard-h1">The Parking Lot</h1>
                     <ul className="dashboard-nav">
@@ -14,11 +14,19 @@ class ParkingLotDashboard extends React.Component {
                         <li><a>Show Questions</a></li>
                     </ul>
                 </header>
-                <div className="question-list">
-                    <QuestionList />
-                </div>
-                <div className="create-question-form">
-                    <CreateQuestionForm/>
+                <div className='main-div'>
+                    <div className='user-profile-details'>
+                        <h1>Ryan's Profile</h1>
+                        <img></img>
+                    </div>
+                    <div className='question-div'>
+                        <div className="question-list">
+                            <QuestionList />
+                        </div>
+                        <div className="create-question-form">
+                            <CreateQuestionForm/>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
